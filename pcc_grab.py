9f30200eea2fc0f43d9af0603ca612dc93ecca62
Allen <kingjames1324@gmail.com>
2017-12-12 17:57:46 +0800

    20171212

--- pcc_grab.py
+++ pcc_grab.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#coding=utf-8
 import pandas as pd
 import csv
-import requests
 from bs4 import BeautifulSoup
-from datetime import date
-
+from selenium import webdriver
 
 url = "http://web.pcc.gov.tw/tps/pss/tender.do?method=goSearch&searchMode=common&searchType=advance&searchTarget=TPAM"
 
-import time
-from selenium import webdriver
+def encode_decode(self):
+    self = self.encode("utf8").decode("cp950", "ignore")
+    # self = self.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding)
+    return self
 
 # windows version
-# driver = webdriver.Chrome(executable_path=r'C:/webdrivers/chromedriver.exe')  # Optional argument, if not specified will search path.
+# driver = webdriver.Chrome(executable_path=r'C:/Webdrivers/chromedriver.exe')  # Optional argument, if not specified will search path.
 # mac os version
 driver = webdriver.Chrome()
 
-driver.get(url);
+driver.get(url)
 # time.sleep(1) # Let the user actually see something!
 search_box = driver.find_element_by_name('tenderName')
 search_box.send_keys('智慧')
@@ -29,9 +30,9 @@ html = driver.page_source
 # time.sleep(5) # Let the user actually see something!
 driver.quit()
 
-f = open("tender.csv","w")
+f = open("tender.csv","w", encoding = 'utf8')
 w = csv.writer(f)
-top_row = [["項次","機關名稱","標案名稱","網址","次數","招標方式","標的分類","公告日期","截止日期"]]
+top_row = [["項次","機關名稱","標案名稱","網址","公告日期","截止日期","金額"]]
 w.writerows(top_row)
 
 soup = BeautifulSoup(html,"lxml")
@@ -54,10 +55,14 @@ for item in form1 :
     category = tenderway.findNext('td')
     start_date = category.findNext('td')
     end_date = start_date.findNext('td')
-    data = [[num.text, facility.text.strip(), title.strip(), url_all.strip(), times.text.strip(), tenderway.text, category.text, start_date.text, end_date.text]]
+    money = end_date.findNext('td')
+    data = [[encode_decode(num.text), facility.text.strip(), title.strip(), \
+    url_all.strip(),\
+    start_date.text, end_date.text, money.text]]
     w.writerows(data)
+    # print(data)
 
-for item in form2:
+for item in form2 :
     num = item.findNext('td')
     facility = num.findNext('td')
     title_old = facility.findNext('td')
@@ -69,8 +74,12 @@ for item in form2:
     category = tenderway.findNext('td')
     start_date = category.findNext('td')
     end_date = start_date.findNext('td')
-    data = [[num.text, facility.text, title, url_all, times.text.strip(), tenderway.text, category.text, start_date.text, end_date.text]]
+    money = end_date.findNext('td')
+    data = [[encode_decode(num.text), facility.text.strip(), title.strip(), \
+    url_all.strip(),\
+    start_date.text, end_date.text, money.text]]
+    # data = [[title.strip()]]
     w.writerows(data)
-
+    # print(data)
 
 f.close()

26bee85141b60f1fe43ed8dade4716e65b8e5af4
Allen <kingjames1324@gmail.com>
2018-03-05 17:53:03 +0800

    google craw temp work

--- google_craw.py
+++ google_craw.py
@@ -21,23 +21,38 @@ def encode_decode(self):
 f = open("google_craw.csv","w", encoding = 'utf8')
 w = csv.writer(f)
 
-# question = "防災+食品+公司"
-question = "NBA"
+question = "防災+食品+公司"
+# question = "NBA"
 
 url_base = "https://www.google.com/search?q="
 url = url_base+question
 
 r = requests.get(url)
 c = r.content
-soup=BeautifulSoup(c,"lxml")
-block = soup.find_all("h3", {"class":"r"})
+# print(r.text)
+# soup=BeautifulSoup(c,"html.parser")
+soup = BeautifulSoup(c, "lxml", from_encoding="big5")
+
+# soup=BeautifulSoup(c,"lxml")
+block = soup.find_all("div", {"class":"g"})
+
+base_url = "https://www.google.com.tw"
+page_tag = soup.find_all("a", {"class":"fl"})
+for item in page_tag:
+    page_url = item.get("href") 
+    
+    print(base_url + page_url)
 
 for item in block:
     title = item.find("a")
-    url_new = title.get("href") 
-    # print(encode_decode(title.text))
+    # url_new = title.get("href") 
+    url_new = item.find('cite')
+    # print(encode_decode(title.text)){"class":"_Rm"}
     print(title.text)
-    data = [[title.text]]
+    print(url_new.text)
+    data = [[title.text, url_new.text]]
     w.writerows(data)
+
+
 
 
